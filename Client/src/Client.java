b060beba58f86510e500f1e96c7945724c4117f1
PBBassily <pbbassily@gmail.com>
2018-02-26 04:51:29 +0200

    updating client logic

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -11,68 +11,82 @@ import java.util.Random;
 
 public class Client {
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args) {
 
-		String serverIP = args[0] ;
+		String serverIP = args[0];
 		int serverPortNum = Integer.parseInt(args[1]);
 		boolean isReader = (Boolean.parseBoolean(args[2])); // true: reader , false: writer
-		int clientNum = Integer.parseInt(args[3]) ;
-		int numberOfAccess =  Integer.parseInt(args[4]);
-		
+		int clientNum = Integer.parseInt(args[3]);
+		int numberOfAccess = Integer.parseInt(args[4]);
 
-		System.out.println("client ID : " + clientNum+
-							"\nserver IP : " +serverIP+
-							"\nserver port :"+serverPortNum+
-							"\nreader : "+isReader+
-							"\naccessNum : "+numberOfAccess);
+		System.out.println("client ID : " + clientNum + "\nserver IP : " + serverIP + "\nserver port :" + serverPortNum
+				+ "\nreader : " + isReader + "\naccessNum : " + numberOfAccess);
 
-		// open output file
-		BufferedWriter fileWriter = new BufferedWriter(new FileWriter("log"+clientNum));
-		if(isReader)
-			fileWriter.append("Client type: Reader\n");
-		else
-			fileWriter.append("Client type: Writer\n");
-		fileWriter.append("Client Name: "+clientNum+"\n");
+		// Open output file
+		BufferedWriter fileWriter = null;
+		try {
+			fileWriter = new BufferedWriter(new FileWriter("log" + clientNum));
 
-		if(isReader)
-			fileWriter.append(String.format("%-10s %-10s %-10s%n", "rSeq" , "sSeq","oVal"));
-		else
-			fileWriter.append(String.format("%-10s %-10s%n", "rSeq" , "sSeq"));
-		
-		
+			if (isReader)
+				fileWriter.append("Client type: Reader\n");
+			else
+				fileWriter.append("Client type: Writer\n");
+			fileWriter.append("Client Name: " + clientNum + "\n");
+
+			if (isReader)
+				fileWriter.append(String.format("%-10s %-10s %-10s%n", "rSeq", "sSeq", "oVal"));
+			else
+				fileWriter.append(String.format("%-10s %-10s%n", "rSeq", "sSeq"));
+		} catch (IOException e) {
+			System.out.println(e + "couldn't open output file");
+		}
+
 		//
 		for (int i = 0; i < numberOfAccess; i++) {
-			Socket socket = new Socket(serverIP, serverPortNum);
-			System.out.println("client "+ clientNum +"connected to server successfully.");
+			Socket socket;
+			try {
+				socket = new Socket(serverIP, serverPortNum);
 
-			PrintWriter wtr = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String response;
-			
-			// Reader
-			if (isReader) {
-				// Send Request 
-				wtr.println("Read " + clientNum);
+				System.out.println("==> " + clientNum +"   connected to server successfully.");
 
-				// Read Response
-				response = reader.readLine();
-				System.out.println("Message received : " + response);				
+				PrintWriter wtr = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String response;
+
+				// Reader
+				if (isReader) {
+					// Send Request
+					wtr.println("Read " + clientNum);
+
+					// Read Response
+					response = reader.readLine();
+					System.out.println("==> " + clientNum +"   Message received : " + response);
+				}
+				// Writer
+				else {
+					int newVal = clientNum;
+					// Send Request
+					wtr.println("Write " + clientNum + " " + newVal);
+
+					// Read Response
+					response = reader.readLine();
+					System.out.println("==> " + clientNum +"   Message received : " + response);
+
+				}
+				fileWriter.append(response + "\n");
+
+			} catch (UnknownHostException e) {
+				System.out.println(e + "==> " + clientNum +"   couldn't find server host");
+			} catch (IOException e) {
+				System.out.println(e + "==> " + clientNum +"   couldn't write into output file");
+
 			}
-			// Writer
-			else {
-				int newVal= clientNum;
-				// Send Request 
-				wtr.println("Write " + clientNum + " "+ newVal );
-				
-				// Read Response
-				response = reader.readLine();
-				System.out.println("Message received : " + response);
-
-			}
-			fileWriter.append(response+"\n");
-
 		}
-		fileWriter.close();
+		try {
+			fileWriter.close();
+		} catch (IOException e) {
+			System.out.println(e + "==> " + clientNum +"   couldn't close output file");
+		}
 
 	}
 }
